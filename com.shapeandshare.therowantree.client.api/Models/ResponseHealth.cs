c88c3c6b3617a2a057c60173ad08370b9c706af6
Joshua C. Burt <joshburt@shapeandshare.com>
2018-12-27 18:00:32 -0700

    place holder

---
+++ com.shapeandshare.therowantree.client.api/Models/ResponseHealth.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace com.shapeandshare.therowantree.client.api.Models
+{
+    public class ResponseHealth
+    {
+        public ResponseHealth()
+        {
+        }
+    }
+}

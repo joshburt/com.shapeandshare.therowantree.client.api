840977c452268365a765095cdd52b9a7fd989aef
Joshua C. Burt <joshburt@shapeandshare.com>
2018-12-29 14:35:14 -0700

    version controller

---
+++ com.shapeandshare.therowantree.client.api/Controllers/VersionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace com.shapeandshare.therowantree.client.api.Controllers
+{
+    [Route("[controller]")]
+    public class VersionController : Controller
+    {
+        public readonly string _version;
+
+        public VersionController(IConfiguration config)
+        {
+            _version = config.GetValue<string>("API_VERSION");
+        }
+
+        // GET: /version
+        [Route("")]
+        [HttpGet]
+        public string Get()
+        {
+            return _version;
+        }
+    }
+}

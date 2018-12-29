466e72a11faaa140548f4bac00258f84ce23d223
Joshua C. Burt <joshburt@shapeandshare.com>
2018-12-29 16:27:49 -0700

    user create working controller

--- com.shapeandshare.therowantree.client.api/Controllers/UserController.cs
+++ com.shapeandshare.therowantree.client.api/Controllers/UserController.cs
@@ -15,5 +15,28 @@ namespace com.shapeandshare.therowantree.client.api.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [Route("create")]
+        public IActionResult UserCreate()
+        {
+            User newUser = new User
+            {
+                Guid = Guid.NewGuid().ToString()
+            };
+
+            try
+            {
+                _context.User.Add(newUser);
+                _context.SaveChanges();
+            }
+            catch (Exception e) {
+                // TODO: log exception.
+                return BadRequest(new ResponseUserCreate { Guid=Guid.Empty, Message="Failed to create new user." });
+            }
+            return Created(newUser.Guid, new ResponseUserCreate { Guid = Guid.Parse(newUser.Guid), Message="" });
+        }
     }
 }

ab996cb3d9766a41bcec2a034fcf021a9f0217e2
Joshua Burt <joshburt@users.noreply.github.com>
2018-12-29 20:34:00 -0700

    User create initial framework (#9)
    * cleanup
    
    * user create working controller
    
    * array of messages!
    
    * wrap each creation piece and report on failures, defaults aren't working expect on the user table, not sure why..
    
    * reduce logic

--- com.shapeandshare.therowantree.client.api/Controllers/UserController.cs
+++ com.shapeandshare.therowantree.client.api/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using com.shapeandshare.therowantree.client.api.Models;
+using System.Linq;
 
 namespace com.shapeandshare.therowantree.client.api.Controllers
 {
@@ -14,6 +15,63 @@ namespace com.shapeandshare.therowantree.client.api.Controllers
         public UserController(TrtDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        [Route("create")]
+        public IActionResult UserCreate()
+        {
+            Guid _guid = Guid.NewGuid();
+            User newUser;
+
+            try
+            {
+                newUser = new User
+                {
+                    Guid = _guid.ToString()
+                };
+                _context.User.Add(newUser);
+                _context.SaveChanges();
+
+                Feature newFeature = new Feature
+                {
+                    UserId = newUser.UserId,
+                    FeatureId = 1
+                };
+                _context.Feature.Add(newFeature);
+
+                UserGameState newUserGameState = new UserGameState
+                {
+                    UserId = newUser.UserId,
+                    ActiveFeature = 1,
+                    GameTemperatureId = 1,
+                    GameFireStateId = 1,
+                    BuilderLevel = -1
+                };
+                _context.UserGameState.Add(newUserGameState);
+
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                // TODO: log exception.
+                return BadRequest(new ResponseUserCreate
+                {
+                    Guid = _guid,
+                    Message = new string[] {
+                            "Unable to commit changes. :(",
+                            e.InnerException.Message
+                        }
+                });
+            }
+
+            return Created(_guid.ToString(), new ResponseUserCreate 
+                { 
+                    Guid = _guid, 
+                    Message = new string[] { }
+                });
         }
     }
 }
---
+++ com.shapeandshare.therowantree.client.api/Models/ResponseUserCreate.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace com.shapeandshare.therowantree.client.api.Models
+{
+    public class ResponseUserCreate
+    {
+        public Guid Guid { get; set; }
+        public string [] Message { get; set; }
+    }
+}

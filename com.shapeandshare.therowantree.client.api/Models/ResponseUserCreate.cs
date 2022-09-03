95e26241073d7e849c46b21825a52fd3473bef64
Joshua Burt <joshburt@users.noreply.github.com>
2022-09-03 16:14:26 -0700

    Jb aug 2022 updates (#13)
    * updates
    
    * refactoring
    
    * updates
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * enhanced dtos
    
    * notes
    
    * object definitions
    
    * additional models
    
    * response fix
    
    * add response type
    
    * linting
    
    * reduce complexity

--- com.shapeandshare.therowantree.client.api/Models/ResponseUserCreate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace com.shapeandshare.therowantree.client.api.Models
-{
-    public class ResponseUserCreate
-    {
-        public Guid Guid { get; set; }
-        public string [] Message { get; set; }
-    }
-}
